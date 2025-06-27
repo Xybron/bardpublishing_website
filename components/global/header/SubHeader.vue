a586a1fd12e338ed3f1c68147537473b79f6d726
Xybron <ronaldnettey@yahoo.com>
2025-06-27 01:00:37 +0000

    Add SubHeader and HeaderBooks components for Books page layout

--- components/global/header/SubHeader.vue
+++ components/global/header/SubHeader.vue
@@ -5,9 +5,14 @@ defineProps<{
 }>()
 </script>
 <template>
-     <section class="h-[40vh] bg-orange-50 flex flex-col items-center justify-center gap-5 relative">
-        <h1 class="font-title text-5xl text-gray-700">{{ title }}</h1>
+     <section class="h-[40vh] bg-green-50 flex flex-col items-center justify-center gap-5 relative">
+       <div class="absolute inset-0 z-0 bg-cover bg-center bg-[url('/imgs/bg_hero.jpg')]">
+            <div class="absolute inset-0 bg-black opacity-70"></div>
+        </div>
+        <div class="relative z-10 text-white text-center">
+
+        <h1 class="font-title text-5xl">{{ title }}</h1>
         <p>{{ subtitle }}</p>
-        <img class="absolute right-0 left-0 w-40 -bottom-10 mx-auto z-10" src="/imgs/leaf-2.png" alt="" />
+        </div>
     </section>
 </template>
---
+++ pages/books.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HeaderBooks from '@/components/global/header/Books.vue';
+import SubHeader from '@/components/global/header/SubHeader.vue';
+
+</script>
+
+<template>
+     <SubHeader :title="'Books'" :subtitle="'Parenting Digital Natives'" />
+ <HeaderBooks/>
+</template>
+
+
+
+
